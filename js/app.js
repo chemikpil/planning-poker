a236d11e3edeb9f2693f9d07d527a694787d9655
ChemikPIL <chemikpil@gmail.com>
2015-06-21 13:39:33 +0200

    Add config

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import App from './components/App.jsx';
 
-var data = ['XS', 'S', 'M', 'L', 'XL', '?'];
+var config = {
+  "type": "tshirt",
+  "cards": {
+    "tshirt": ['XS', 'S', 'M', 'L', 'XL', '?'],
+    "fibonacci": ['0', '0.5', '1', '2', '3', '5', '8', '13', '21', '40', '100', '?']
+  }
+}
 
 React.render(
-  <App data={data} />,
+  <App data={config} />,
   document.body
 );
--- js/components/App.jsx
+++ js/components/App.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import Cards from './Cards.jsx';
 
 class App extends React.Component {
+  constructor (props) {
+    super();
+    this.state = {
+      type: props.data.type
+    };
+  }
+  
   render () {
+    var cards = this.props.data.cards[this.state.type];
     return (
       <div className="page" data-page="cards">
         <img src="img/logo.png" width="320" alt="Planning poker" />
-      
-        <Cards cards={this.props.data} />
+        <Cards cards={cards} />
       </div>
     );
   }
